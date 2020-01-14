5c4971979d8366af6469cbf4d24fcc80d30d47c1
antran1111 <54298061+antran1111@users.noreply.github.com>
2020-01-14 12:42:24 -0800

    Update networking.dart

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -12,14 +12,7 @@ class NetworkHelper {
     http.Response response = await http.get(
       //hold down cmd or windows and click on get, open up the get method from the http package file
         '$url');
-    //  print(response.body); //where the data is stored
-    // print(response.statusCode);
-    //gives code 200. 200 means success, 404 is missing., 4xx is client error
-    //5xx is server error
-    //3xx is private
-    //lookup http return status code for more info
 
-    //check for errors
     if (response.statusCode == 200) {
       String data = response.body;
       var longitude = jsonDecode(data)['coord']['lon'];
@@ -28,17 +21,6 @@ class NetworkHelper {
       var weatherDescription = jsonDecode(data)['weather'][0]['description'];
       print(weatherDescription);
 
-      /*     var weatherT = jsonDecode(data) ['main']['temp'];
-      print('Temperature: ' + weatherT.toString());
-
-      var weatherH = jsonDecode(data) ['main']['humidity'];
-      print('Humidity: $weatherH');
-
-      var weatherL = jsonDecode(data) ['name'];
-      print('Location: ' + weatherL);*/
-//the way below is easier
-      //hoover over data, press control + j to find datatype, dynamic
-      //can go to json and change the variable types base on what you see
       var decodedData = jsonDecode(data);
       return decodedData;
     } else {
