2efbf532a6ada934ef7ef9b8875858d72881847d
antran1111 <54298061+antran1111@users.noreply.github.com>
2020-01-14 04:46:00 -0800

    addded html package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:climate_v1_mac/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +10,40 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getData();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    location.getCurrentLocation();
+
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  //getData method is from the http package
+/*void getData() {
+    get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22')
+
+}*/
+//turn getData into an async response
+ void getData()  async {
+    Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22')
+;
+  }
+
+  @override
   Widget build(BuildContext context) {
+   getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
