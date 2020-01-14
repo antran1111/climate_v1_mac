b5b5ffd01da0090232a119a989c8975114bf3cce
antran1111 <54298061+antran1111@users.noreply.github.com>
2020-01-14 05:11:57 -0800

    import 'package:http/http.dart' as http;
    as http makes it easier to use this package and not confuse it with other methods that we made.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http; //as http, makes it easier to use package and not confuse names (get) with other methods
+import 'package:geolocator/geolocator.dart' ;
 import 'package:climate_v1_mac/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -18,7 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
     Location location = Location();
-    location.getCurrentLocation();
+    await location.getCurrentLocation();
 
     print(location.latitude);
     print(location.longitude);
@@ -30,14 +30,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
 }*/
 //turn getData into an async response
- void getData()  async {
-    Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22')
-;
+  void getData() async {
+    http.Response response = await http.get( //hold down cmd or windows and click on get, open up the get method from the http package file
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+  //  print(response.body); //where the data is stored
+   // print(response.statusCode);
+    //gives code 200. 200 means success, 404 is missing., 4xx is client error
+    //5xx is server error
+    //3xx is private
+    //lookup http return status code for more info
+
+    //check for errors
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-   getData();
+    getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
